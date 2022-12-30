4119ba5a24281d629280fc79c2da23c30473bad8
Klaudija19 <klaudija.kondovska@gmail.com>
2022-12-30 12:19:04 +0100

    First commit

---
+++ src/zadaca1/Glavna.java
@@ -0,0 +1,14 @@
+package zadaca1;
+
+import java.io.IOException;
+
+public class Glavna {
+	
+	public static void main(String[] args) throws IOException {
+		Datoteka d = new Datoteka ("file.txt");
+		d.zapishi("Tekstot");
+		d.prochitaj();
+	}
+}
+
+
---
+++ src/zadaca3/Glavna.java
@@ -0,0 +1,17 @@
+package zadaca3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Glavna {
+	
+	public static void main(String[] args) {
+		XmlCreator xml = new XmlCreator();
+		List<Student> s = new ArrayList<Student>();
+		s.add(new Student("Marko", "Jovanovski", 100));
+		s.add(new Student("Stefan", "Ivanovski", 105));
+		xml.createXMLFile("xml.xml", s);
+	}
+}
+
+
